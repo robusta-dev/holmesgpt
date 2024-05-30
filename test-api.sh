6f887208a9882d5323800f105ec5a97c9388824d
Robert Szefler <robert@robusta.dev>
2024-05-30 15:48:14 +0200

    Some missing code

--- test-api.sh
+++ test-api.sh
@@ -1,5 +1,6 @@
 curl -XPOST 127.0.0.1:8000/api/investigate -H "Content-Type: application/json" --data "{
   \"source\": \"prometheus\",
+  \"source_instance_id\": \"some-instance\",
   \"title\": \"Pod is crash looping.\",
   \"description\": \"Pod default/oomkill-deployment-696dbdbf67-d47z6 (main2) is in waiting state (reason: \\\"CrashLoopBackOff\\\").\",
   \"subject\": {
@@ -17,7 +18,7 @@ curl -XPOST 127.0.0.1:8000/api/investigate -H "Content-Type: application/json" -
   \"context\": [
     {
       \"type\": \"robusta_issue_id\",
-      \"value\": \"0xdeadbeef\"
+      \"value\": \"5b3e2fb1-cb83-45ea-82ec-318c94718e44\"
     }
   ]
 }"
