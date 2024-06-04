3b0012fb4477c103048031f562c4a15ee7d327c8
Robert Szefler <robert@robusta.dev>
2024-06-04 13:50:48 +0200

    Add tool call info in API responses if requested

--- test-api.sh
+++ test-api.sh
@@ -20,5 +20,7 @@ curl -XPOST 127.0.0.1:8000/api/investigate -H "Content-Type: application/json" -
       \"type\": \"robusta_issue_id\",
       \"value\": \"5b3e2fb1-cb83-45ea-82ec-318c94718e44\"
     }
-  ]
+  ],
+  \"include_tool_calls\": true,
+  \"include_tool_call_results\": true
 }"
