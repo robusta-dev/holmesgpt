081359b0f60b8e17a26b7ea351a9437260402afe
Robusta Runner <aantny@gmail.com>
2024-06-24 15:36:53 +0300

    Make tool calling compatible with gemini

--- holmes/core/tools.py
+++ holmes/core/tools.py
@@ -11,7 +11,6 @@ from pydantic import BaseModel, ConfigDict, PrivateAttr
 ToolsetPattern = Union[Literal['*'], List[str]]
 
 class ToolParameter(BaseModel):
-    title: Optional[str] = None
     description: Optional[str] = None
     type: str = "string"
     required: bool = True
@@ -20,7 +19,6 @@ class ToolParameter(BaseModel):
 class YAMLTool(BaseModel):
     name: str
     description: str
-    title: Optional[str] = None
     command: Optional[str] = None
     script: Optional[str] = None
     parameters: Dict[str, ToolParameter] = {}
@@ -49,12 +47,9 @@ class YAMLTool(BaseModel):
         tool_properties = {}
         for param_name, param_attributes in self.parameters.items():
             tool_properties[param_name] = { "type": param_attributes.type }
-            tool_properties[param_name]["title"] = param_attributes.title or param_name
             if param_attributes.description is not None:
                 tool_properties[param_name]["description"] = param_attributes.description
         
-        title = self.title or self.name
-
         result = {
             "type": "function",
             "function": {
@@ -63,7 +58,6 @@ class YAMLTool(BaseModel):
                 "parameters": { 
                     "properties": tool_properties, 
                     "required": [param_name for param_name, param_attributes in self.parameters.items() if param_attributes.required],
-                    "title": title,
                     "type": "object", 
                 }
             },
