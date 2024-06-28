7def5acac5c0bc589de05c9d8b69d5994986c9e0
Robusta Runner <aantny@gmail.com>
2024-06-28 23:39:30 +0300

    start using dunamai for versioning

--- holmes/__init__.py
+++ holmes/__init__.py
@@ -1,12 +1,16 @@
 import os
 import subprocess
 import sys
+import logging
+from pathlib import Path
+import dunamai as dunamai
 
 # For relative imports to work in Python 3.6 - see https://stackoverflow.com/a/49375740
-this_path = os.path.dirname(os.path.realpath(__file__)) 
+this_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(this_path)
 
 # This is patched by github actions during release
+# TODO: now that we are using dunamai we can probably do something better with it and not patch in github actions
 __version__ = "0.0.0"
 
 
@@ -14,20 +18,11 @@ def get_version() -> str:
     # the version string was patched by a release - return __version__ which will be correct
     if not __version__.startswith("0.0.0"):
         return __version__
-    
-    # we are running from an unreleased dev version
-    try:
-        # Get the latest git tag
-        tag = subprocess.check_output(["git", "describe", "--tags"], stderr=subprocess.STDOUT, cwd=this_path).decode().strip()
 
-        # Get the current branch name
-        branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"], stderr=subprocess.STDOUT, cwd=this_path).decode().strip()
-
-        # Check if there are uncommitted changes
-        status = subprocess.check_output(["git", "status", "--porcelain"], stderr=subprocess.STDOUT, cwd=this_path).decode().strip()
-        dirty = "-dirty" if status else ""
-
-        return f"{tag}-{branch}{dirty}"
-    
-    except Exception:
-        return f"dev-version"
+    version = dunamai.get_version(
+        "holmes-gpt",
+        first_choice=lambda: dunamai.Version.from_git(
+            pattern=dunamai.Pattern.DefaultUnprefixed, path=this_path
+        ),
+    )
+    return version.serialize(format="{base}-{branch}-{commit}-{dirty}")
