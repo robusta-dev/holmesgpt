86b8498723d820dc4695d7bee5fac451500fbc6a
Arik Alon <alon.arik@gmail.com>
2024-07-22 17:24:32 +0300

    remove check on model name

--- holmes/core/tool_calling_llm.py
+++ holmes/core/tool_calling_llm.py
@@ -122,8 +122,8 @@ class ToolCallingLLM:
 
             tools_to_call = getattr(response_message, "tool_calls", None)
             if not tools_to_call:
-                # claude is a chatty model so we automatically post process it
-                if post_process_prompt or "claude" in self.model:
+                # For chatty models post process and summarize the result
+                if post_process_prompt:
                     logging.info(f"Running post processing on investigation.")
                     raw_response = response_message.content
                     post_processed_response = self._post_processing_call(prompt=user_prompt, investigation=raw_response, user_prompt=post_process_prompt)
