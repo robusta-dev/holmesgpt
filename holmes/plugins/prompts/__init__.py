67934a94a1c89e94448f21690ff362d8136ee39f
Natan Yellin <aantn@users.noreply.github.com>
2024-09-22 19:50:27 +0300

    Reuse prompts (#139)
    Use jinja2 template includes so we don't repeat prompts everywhere.
    
    Tests
    
    - [x] Test holmes ask
    - [ ] Test holmes in server mode and integrated with runner - test all 3
    prompts used that way (investigate alert, ask follow up question, and
    check for microservice issue)
    - [ ] Test a holmes investigate command

--- holmes/plugins/prompts/__init__.py
+++ holmes/plugins/prompts/__init__.py
@@ -1,11 +1,12 @@
 import os
 import os.path
+from jinja2 import Environment, FileSystemLoader
 
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def load_prompt(prompt: str) -> str:
     """
-    filename is either in the format 'builtin://' or 'file://' or a regular string
+    prompt is either in the format 'builtin://' or 'file://' or a regular string
     builtins are loaded as a file from this directory
     files are loaded from the file system normally
     regular strings are returned as is (as literal strings)
@@ -18,3 +19,22 @@ def load_prompt(prompt: str) -> str:
         return prompt
     
     return open(path).read()
+
+def load_and_render_prompt(prompt: str, context: dict = None) -> str:
+    """
+    prompt is in the format 'builtin://' or 'file://' or a regular string
+    see load_prompt() for details
+
+    context is a dictionary of variables to be passed to the jinja2 template
+    """
+    prompt_as_str = load_prompt(prompt)
+
+    env = Environment(
+        loader=FileSystemLoader(THIS_DIR),
+    )
+    template = env.from_string(prompt_as_str)
+
+    if context is None:
+        context = {}
+
+    return template.render(**context)
