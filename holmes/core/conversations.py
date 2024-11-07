5059455fcc93810c47f6c34f92bbd32826bf24d4
Dima Chievtaiev <dmitrychevtaev.dev@gmail.com>
2024-11-07 09:30:46 +0100

    Added case for calculate_tool_size when number_of_tools is 0

--- holmes/core/conversations.py
+++ holmes/core/conversations.py
@@ -14,10 +14,13 @@ from holmes.core.tool_calling_llm import ToolCallingLLM
 def calculate_tool_size(
     ai: ToolCallingLLM, messages_without_tools: list[dict], number_of_tools: int
 ) -> int:
+    if number_of_tools == 0:
+        return 10000
+    
     context_window = ai.llm.get_context_window_size()
     message_size_without_tools = ai.llm.count_tokens_for_message(messages_without_tools)
     maximum_output_token = ai.llm.get_maximum_output_token()
-
+    
     tool_size = min(
         10000,
         int(
