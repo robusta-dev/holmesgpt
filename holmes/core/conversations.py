4b8cb5b71284f4818224d9c3f67c2edcbb6b71bb
itisallgood <25401000+itisallgood@users.noreply.github.com>
2024-11-07 11:17:14 +0100

    Fix build_chat_message when number of tools is 0 (#186)

--- holmes/core/conversations.py
+++ holmes/core/conversations.py
@@ -11,15 +11,20 @@ from holmes.plugins.prompts import load_and_render_prompt
 from holmes.core.tool_calling_llm import ToolCallingLLM
 
 
+DEFAULT_TOOL_SIZE = 10000
+
 def calculate_tool_size(
     ai: ToolCallingLLM, messages_without_tools: list[dict], number_of_tools: int
 ) -> int:
+    if number_of_tools == 0:
+        return DEFAULT_TOOL_SIZE
+    
     context_window = ai.llm.get_context_window_size()
     message_size_without_tools = ai.llm.count_tokens_for_message(messages_without_tools)
     maximum_output_token = ai.llm.get_maximum_output_token()
-
+    
     tool_size = min(
-        10000,
+        DEFAULT_TOOL_SIZE,
         int(
             (context_window - message_size_without_tools - maximum_output_token)
             / number_of_tools
@@ -326,7 +331,7 @@ def build_chat_messages(
         [message for message in conversation_history if message.get("role") == "tool"]
     )
     if number_of_tools == 0:
-        return messages
+        return conversation_history
 
     conversation_history_without_tools = [
         message for message in conversation_history if message.get("role") != "tool"
