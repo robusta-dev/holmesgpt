92acc09eb3e9fd27536a8d43763f6e39edc6dec0
Nicolas Herment <nherment@arkhaios.net>
2024-11-19 12:31:52 +0100

    test: loosen llm test success criteria

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -15,7 +15,7 @@ from tests.utils import AskHolmesTestCase, load_ask_holmes_test_cases
 
 TEST_CASES_FOLDER = Path("tests/fixtures/test_chat")
 
-test_cases = load_ask_holmes_test_cases(TEST_CASES_FOLDER, expected_number_of_test_cases=7)
+test_cases = load_ask_holmes_test_cases(TEST_CASES_FOLDER)
 
 
 def idfn(test_case:AskHolmesTestCase):
