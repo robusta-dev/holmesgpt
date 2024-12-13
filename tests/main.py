75de978d40279116627166ae079bdaa71ea70274
Nicolas Herment <nherment@arkhaios.net>
2024-12-13 15:11:56 +0100

    feat: kafka

--- tests/main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from tests.utils.kafka import wait_for_containers
-
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-FIXTURE_FOLDER = os.path.join(dir_path, "fixtures", "test_tool_kafka")
-wait_for_containers(FIXTURE_FOLDER)
