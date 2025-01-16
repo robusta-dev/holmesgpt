abbd3f3ba4cab9640ec5c2c183d93d97ce13a2a1
avi@robusta.dev <avi@robusta.dev>
2025-01-16 15:36:21 +0200

    elasticsearch

--- holmes/config.py
+++ holmes/config.py
@@ -35,8 +35,39 @@ from holmes.utils.definitions import CUSTOM_TOOLSET_LOCATION
 from holmes.utils.holmes_sync_toolsets import load_custom_toolsets_config, merge_and_override_bultin_toolsets_with_toolsets_config
 from holmes.common.env_vars import ROBUSTA_CONFIG_PATH
 from holmes.utils.definitions import RobustaConfig
+import re
 
 DEFAULT_CONFIG_LOCATION = os.path.expanduser("/Users/avirobusta/git/holmesgpt/config.yaml")
+
+def get_env_replacement(value: str) -> Optional[str]:
+    logging.warning(f"value {value}")
+    env_values = re.findall(r"{{[ ]*env\.(.*)[ ]*}}", value)
+    if env_values:
+        env_var_value = os.environ.get(env_values[0].strip(), None)
+        if not env_var_value:
+            msg = f"ENV var replacement {env_values[0]} does not exist for param: {value}"
+            logging.error(msg)
+            raise Exception(msg)
+        return env_var_value
+    return None
+
+
+def replace_env_vars_values(values: Dict) -> Dict:
+    for key, value in values.items():
+        if isinstance(value, str):
+            env_var_value = get_env_replacement(value)
+            if env_var_value:
+                values[key] = env_var_value
+        elif isinstance(value, SecretStr):
+            env_var_value = get_env_replacement(value.get_secret_value())
+            if env_var_value:
+                values[key] = SecretStr(env_var_value)
+        elif isinstance(value, dict):
+            env_var_value = replace_env_vars_values(value)
+            if env_var_value:
+                values[key] = env_var_value
+
+    return values
 
 class Config(RobustaBaseConfig):
     api_key: Optional[SecretStr] = (
