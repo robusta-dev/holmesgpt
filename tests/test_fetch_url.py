49bcf04305e4d3ce05f9c8f5c171e2dc6359103d
Mohse Morad <moshemorad12340@gmail.com>
2025-01-23 10:30:34 +0200

    Fix tests

--- tests/test_fetch_url.py
+++ tests/test_fetch_url.py
@@ -7,7 +7,7 @@ from holmes.core.tools import ToolExecutor, ToolsetStatusEnum
 from holmes.plugins.toolsets.internet import InternetToolset, html_to_markdown
 
 THIS_DIR = os.path.dirname(__file__)
-FIXTURES_DIR = os.path.join(THIS_DIR, 'fixtures', 'test_fetch_url')
+FIXTURES_DIR = os.path.join(THIS_DIR, "fixtures", "test_fetch_url")
 
 
 TEST_URL = "https://www.example.com"
@@ -23,17 +23,20 @@ This domain is for use in illustrative examples in documents. You may use this
 More information...
 """.strip()
 
-def read_file(file_path:Path):
-    with open(file_path, 'r', encoding='utf-8') as file:
+
+def read_file(file_path: Path):
+    with open(file_path, "r", encoding="utf-8") as file:
         return file.read().strip()
 
-def parse_fixture_id(file_name:str) -> str:
-    match = re.match(r'fixture(\d+)', file_name)
+
+def parse_fixture_id(file_name: str) -> str:
+    match = re.match(r"fixture(\d+)", file_name)
     if match:
         # Extract the number
         return match.group(1)
     else:
         raise Exception(f"Could not find fixture id in filename {file_name}")
+
 
 def load_all_fixtures():
     """
@@ -41,12 +44,12 @@ def load_all_fixtures():
     to feed the test
     """
     fixtures_dir = Path(FIXTURES_DIR)
-    input_files = sorted([f for f in fixtures_dir.glob('fixture*.html')])
+    input_files = sorted([f for f in fixtures_dir.glob("fixture*.html")])
     test_cases = []
 
     for input_file in input_files:
         number = parse_fixture_id(input_file.stem)
-        output_file = fixtures_dir / f'fixture{number}_output.md'
+        output_file = fixtures_dir / f"fixture{number}_output.md"
 
         if output_file.exists():
             input_content = read_file(input_file)
@@ -55,6 +58,7 @@ def load_all_fixtures():
 
     assert len(test_cases) > 0
     return test_cases
+
 
 @pytest.mark.parametrize("input,expected_output", load_all_fixtures())
 def test_html_to_markdown(input, expected_output):
@@ -66,13 +70,19 @@ def test_internet_toolset_prerequisites():
     toolset = InternetToolset()
 
     toolset.check_prerequisites()
-    assert toolset._status == ToolsetStatusEnum.ENABLED, ("" if  toolset._status == ToolsetStatusEnum.ENABLED else toolset.get_error() + ". Make sure playwright is installed by running `playwright install`.")
+    assert toolset._status == ToolsetStatusEnum.ENABLED, (
+        ""
+        if toolset._status == ToolsetStatusEnum.ENABLED
+        else toolset.get_error()
+        + ". Make sure playwright is installed by running `playwright install`."
+    )
 
 
 def test_fetch_webpage():
     toolset = InternetToolset()
+    toolset._status = ToolsetStatusEnum.ENABLED
     tool_executor = ToolExecutor(toolsets=[toolset])
-    fetch_webpage_tool = tool_executor.get_tool_by_name('fetch_webpage')
+    fetch_webpage_tool = tool_executor.get_tool_by_name("fetch_webpage")
     assert fetch_webpage_tool
     actual_output = fetch_webpage_tool.invoke({"url": TEST_URL})
     assert actual_output.strip() == EXPECTED_TEST_RESULT
