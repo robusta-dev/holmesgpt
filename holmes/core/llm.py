cd37332def5474ae318f0d522396c49276ce0537
avi@robusta.dev <avi@robusta.dev>
2025-02-16 09:04:04 +0200

    add override header

--- holmes/core/llm.py
+++ holmes/core/llm.py
@@ -165,6 +165,10 @@ class DefaultLLM(LLM):
         temperature: Optional[float] = None,
         drop_params: Optional[bool] = None,
     ) -> ModelResponse:
+        headers = {}
+        if os.getenv("OVERRIDE_API_KEY",None):
+            key = os.getenv("OVERRIDE_API_KEY",None)
+            headers["Authorization"] = f"Bearer {key}"
         result = litellm.completion(
             model=self.model,
             api_key=self.api_key,
@@ -175,6 +179,7 @@ class DefaultLLM(LLM):
             temperature=temperature,
             response_format=response_format,
             drop_params=drop_params,
+            headers=headers
         )
 
         if isinstance(result, ModelResponse):
