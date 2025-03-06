6262e8432b10b4a5f3789a908e79debd585e2b67
Nicolas Herment <nherment@arkhaios.net>
2025-03-06 08:36:19 +0100

    chore: static checks

--- tests/test_grafana_common.py
+++ tests/test_grafana_common.py
@@ -2,38 +2,79 @@ import pytest
 from dateutil import parser
 from holmes.plugins.toolsets.grafana.common import process_timestamps
 
+
 class TestProcessTimestamps:
-
-    @pytest.mark.parametrize("start_timestamp, end_timestamp, expected_start, expected_end", [
-
-        # Integer timestamps
-        (1600000000, 1600003600, "2020-09-13T12:26:40+00:00", "2020-09-13T13:26:40+00:00"),
-
-        # RFC3339 formatted timestamps
-        ("2020-09-13T12:26:40+00:00", "2020-09-13T13:26:40+00:00", "2020-09-13T12:26:40+00:00", "2020-09-13T13:26:40+00:00"),
-
-        # Negative start integer as relative time to Unix
-        (-3600, 1600003600, "2020-09-13T12:26:40+00:00", "2020-09-13T13:26:40+00:00"),
-        # Negative start integer as relative time to RFC3339
-        (-300, "2020-09-13T12:26:40+00:00", "2020-09-13T12:21:40+00:00", "2020-09-13T12:26:40+00:00"),
-        # Auto inversion, Negative end integer as relative time to RFC3339
-        ("2020-09-13T12:26:40+00:00", -300, "2020-09-13T12:21:40+00:00", "2020-09-13T12:26:40+00:00"),
-
-        # Auto-inversion if start is after end
-        (1600003600, 1600000000, "2020-09-13T12:26:40+00:00", "2020-09-13T13:26:40+00:00"),
-
-        # String integers
-        ("1600000000", "1600003600", "2020-09-13T12:26:40+00:00", "2020-09-13T13:26:40+00:00"),
-
-        # Mixed format (RFC3339 + Unix)
-        ("2020-09-13T12:26:40+00:00", 1600003600, "2020-09-13T12:26:40+00:00", "2020-09-13T13:26:40+00:00"),
-
-        # Mixed format (Unix + RFC3339)
-        (1600000000, "2020-09-13T13:26:40+00:00", "2020-09-13T12:26:40+00:00", "2020-09-13T13:26:40+00:00"),
-
-    ])
-    def test_process_timestamps(self, start_timestamp, end_timestamp, expected_start, expected_end):
-
+    @pytest.mark.parametrize(
+        "start_timestamp, end_timestamp, expected_start, expected_end",
+        [
+            # Integer timestamps
+            (
+                1600000000,
+                1600003600,
+                "2020-09-13T12:26:40+00:00",
+                "2020-09-13T13:26:40+00:00",
+            ),
+            # RFC3339 formatted timestamps
+            (
+                "2020-09-13T12:26:40+00:00",
+                "2020-09-13T13:26:40+00:00",
+                "2020-09-13T12:26:40+00:00",
+                "2020-09-13T13:26:40+00:00",
+            ),
+            # Negative start integer as relative time to Unix
+            (
+                -3600,
+                1600003600,
+                "2020-09-13T12:26:40+00:00",
+                "2020-09-13T13:26:40+00:00",
+            ),
+            # Negative start integer as relative time to RFC3339
+            (
+                -300,
+                "2020-09-13T12:26:40+00:00",
+                "2020-09-13T12:21:40+00:00",
+                "2020-09-13T12:26:40+00:00",
+            ),
+            # Auto inversion, Negative end integer as relative time to RFC3339
+            (
+                "2020-09-13T12:26:40+00:00",
+                -300,
+                "2020-09-13T12:21:40+00:00",
+                "2020-09-13T12:26:40+00:00",
+            ),
+            # Auto-inversion if start is after end
+            (
+                1600003600,
+                1600000000,
+                "2020-09-13T12:26:40+00:00",
+                "2020-09-13T13:26:40+00:00",
+            ),
+            # String integers
+            (
+                "1600000000",
+                "1600003600",
+                "2020-09-13T12:26:40+00:00",
+                "2020-09-13T13:26:40+00:00",
+            ),
+            # Mixed format (RFC3339 + Unix)
+            (
+                "2020-09-13T12:26:40+00:00",
+                1600003600,
+                "2020-09-13T12:26:40+00:00",
+                "2020-09-13T13:26:40+00:00",
+            ),
+            # Mixed format (Unix + RFC3339)
+            (
+                1600000000,
+                "2020-09-13T13:26:40+00:00",
+                "2020-09-13T12:26:40+00:00",
+                "2020-09-13T13:26:40+00:00",
+            ),
+        ],
+    )
+    def test_process_timestamps(
+        self, start_timestamp, end_timestamp, expected_start, expected_end
+    ):
         result_start, result_end = process_timestamps(start_timestamp, end_timestamp)
 
         # For time-dependent tests, we allow a small tolerance
