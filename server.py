8a7a2f5df88182ecb9c60618af2b3598630eeeab
Roi Glinik <groi.tech@gmail.com>
2025-03-10 15:40:21 +0200

    add exception check for server endpoint

--- server.py
+++ server.py
@@ -150,9 +150,13 @@ def investigate_issues(investigate_request: InvestigateRequest):
 def investigate_issues1(req: InvestigateRequest):
     ai, system_prompt, user_prompt, response_format, sections = investigation.get_investigation_context(req, dal, config=config)
 
-    # TODO handle errors
-    return StreamingResponse(ai.call_stream(system_prompt, user_prompt, response_format), media_type='text/event-stream')
-
+    try:
+        return StreamingResponse(ai.call_stream(system_prompt, user_prompt, response_format), media_type='text/event-stream')
+    except AuthenticationError as e:
+        raise HTTPException(status_code=401, detail=e.message)
+    except Exception as e:
+        logging.exception(f"Error in /api/stream/investigate: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/api/workload_health_check")
 def workload_health_check(request: WorkloadHealthRequest):
