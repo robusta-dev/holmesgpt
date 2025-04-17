465bfbb97483e11225a1a59dd9e80e514449a542
Roi Glinik <groi.tech@gmail.com>
2025-04-17 11:50:33 +0300

    fix test case

--- tests/config_class/test_config_load_model_credentials.py
+++ tests/config_class/test_config_load_model_credentials.py
@@ -11,7 +11,9 @@ def test_load_custom_toolsets_config_valid(monkeypatch, tmp_path):
         "bedrock": {"model": "bbbb", "api-key": "asfffd"},
     }
     temp_config_file.write_text(yaml.dump(data))
-    monkeypatch.setenv("MODEL_CREDENTIALS_LOCATION", str(temp_config_file))
+    monkeypatch.setattr(
+        "holmes.config.MODEL_CREDENTIALS_LOCATION", str(temp_config_file)
+    )
     monkeypatch.setenv("TEST", "test-value")
 
     config = Config()
