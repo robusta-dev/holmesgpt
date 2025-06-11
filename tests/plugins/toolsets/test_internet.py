2cf1465f9c074ce750ada92e498007f1c331181f
Roi Glinik <groi.tech@gmail.com>
2025-06-11 10:47:49 +0300

    ROB-1413 CVE packages updates (#504)
    Updated packages.

--- tests/plugins/toolsets/test_internet.py
+++ tests/plugins/toolsets/test_internet.py
@@ -21,7 +21,7 @@ Example Domain
 ==============
 
 This domain is for use in illustrative examples in documents. You may use this
- domain in literature without prior coordination or asking for permission.
+domain in literature without prior coordination or asking for permission.
 
 [More information...](https://www.iana.org/domains/example)
 """.strip()
