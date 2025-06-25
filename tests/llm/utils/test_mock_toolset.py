dc591cc8c7d7b31593a767689180323fbbdc1e9d
Nicolas Herment <nherment@arkhaios.net>
2025-06-25 14:43:10 +0200

    test: skip test that requires a kubernetes env

--- tests/llm/utils/test_mock_toolset.py
+++ tests/llm/utils/test_mock_toolset.py
@@ -95,7 +95,7 @@ def assert_toolset_enabled(mock_toolsets: MockToolsets, toolset_name: str):
     assert False, f"Expected toolset {toolset_name} to be enabled but it missing from the list of enabled toolsets"
 
 
-@pytest.skip("Test fail on github because kubernetes is not available")
+@pytest.mark.skip(reason="Test fail on github because kubernetes is not available")
 def test_enabled_toolsets():
     # This test ensures `MockToolsets` behaves like HolmesGPT and that it returns the same
     # list of enabled toolsets as HolmesGPT in production
