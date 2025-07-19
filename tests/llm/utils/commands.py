3df23e4825122466ceda1b794165c14f22d1e602
Robusta Runner <aantny@gmail.com>
2025-07-19 11:24:34 +0300

    fix eval 28 and add ability to override environment variables for specific evals

--- tests/llm/utils/commands.py
+++ tests/llm/utils/commands.py
@@ -2,6 +2,8 @@
 import logging
 import os
 import subprocess
+from contextlib import contextmanager
+from typing import Dict, Optional
 from tests.llm.utils.mock_utils import HolmesTestCase
 
 
@@ -46,3 +48,33 @@ def after_test(test_case: HolmesTestCase):
         commands = test_case.after_test.split("\n")
         for command in commands:
             invoke_command(command=command, cwd=test_case.folder)
+
+
+@contextmanager
+def test_env_vars(test_case: HolmesTestCase):
+    """Context manager to set and restore environment variables for test execution."""
+    if not test_case.test_env_vars:
+        yield
+        return
+
+    # Save current environment variable values
+    saved_env_vars: Dict[str, Optional[str]] = {}
+    for key in test_case.test_env_vars.keys():
+        saved_env_vars[key] = os.environ.get(key)
+
+    try:
+        # Set test environment variables
+        for key, value in test_case.test_env_vars.items():
+            os.environ[key] = value
+
+        yield
+    finally:
+        # Restore original environment variable values
+        for key, original_value in saved_env_vars.items():
+            if original_value is None:
+                # Variable didn't exist before, remove it
+                if key in os.environ:
+                    del os.environ[key]
+            else:
+                # Variable existed before, restore original value
+                os.environ[key] = original_value
