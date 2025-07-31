8fad3b296b22ff3e3d888daab69f2e8237ff57e7
Roi Glinik <groi.tech@gmail.com>
2025-07-31 10:36:34 +0300

    mypy fix

--- holmes/utils/stream.py
+++ holmes/utils/stream.py
@@ -16,7 +16,7 @@ class StreamMessage(BaseModel):
     data: dict = Field(default={})
 
 
-def create_sse_message(event_type: str, data: dict = None):
+def create_sse_message(event_type: str, data: Optional[dict] = None):
     if data is None:
         data = {}
     return f"event: {event_type}\ndata: {json.dumps(data)}\n\n"
