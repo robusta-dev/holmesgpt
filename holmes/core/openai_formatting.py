74cb7a7930584df893b624c6e3f377901b3a9da9
Nicolas Herment <nherment@arkhaios.net>
2025-08-08 07:58:44 +0200

    wip

--- holmes/core/openai_formatting.py
+++ holmes/core/openai_formatting.py
@@ -1,21 +1,29 @@
 import re
+from typing import Any
 
 # parses both simple types: "int", "array", "string"
 # but also arrays of those simpler types: "array[int]", "array[string]", etc.
 pattern = r"^(array\[(?P<inner_type>\w+)\])|(?P<simple_type>\w+)$"
 
 
-def type_to_open_ai_schema(type_value):
-    match = re.match(pattern, type_value.strip())
+def type_to_open_ai_schema(param_attributes:Any):
+    match = re.match(pattern, param_attributes.type.strip())
 
+    type_obj = None
     if not match:
-        raise ValueError(f"Invalid type format: {type_value}")
+        raise ValueError(f"Invalid type format: {param_attributes.type.strip()}")
 
     if match.group("inner_type"):
-        return {"type": "array", "items": {"type": match.group("inner_type")}}
+        type_obj = {"type": "array", "items": {"type": match.group("inner_type")}}
 
     else:
-        return {"type": match.group("simple_type")}
+        type_obj = {"type": match.group("simple_type")}\
+        
+        
+    if not param_attributes.required:
+        type_obj["type"] = [type_obj["type"], "null"]
+
+    return type_obj
 
 
 def format_tool_to_open_ai_standard(
@@ -23,7 +31,7 @@ def format_tool_to_open_ai_standard(
 ):
     tool_properties = {}
     for param_name, param_attributes in tool_parameters.items():
-        tool_properties[param_name] = type_to_open_ai_schema(param_attributes.type)
+        tool_properties[param_name] = type_to_open_ai_schema(param_attributes)
         if param_attributes.description is not None:
             tool_properties[param_name]["description"] = param_attributes.description
 
@@ -37,15 +45,16 @@ def format_tool_to_open_ai_standard(
                 "required": [
                     param_name
                     for param_name, param_attributes in tool_parameters.items()
-                    if param_attributes.required
                 ],
                 "type": "object",
+                "additionalProperties": False
             },
+            "strict": True,
         },
     }
 
     # gemini doesnt have parameters object if it is without params
-    if tool_properties is None or tool_properties == {}:
-        result["function"].pop("parameters")  # type: ignore
+    # if tool_properties is None or tool_properties == {}:
+    #     result["function"].pop("parameters")  # type: ignore
 
     return result
