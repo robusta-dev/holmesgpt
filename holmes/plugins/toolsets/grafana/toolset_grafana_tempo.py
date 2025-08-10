e71c9e60559f09249a529c846c900f2921cf83e0
Robusta Runner <aantny@gmail.com>
2025-08-10 12:45:26 +0300

    fix

--- holmes/plugins/toolsets/grafana/toolset_grafana_tempo.py
+++ holmes/plugins/toolsets/grafana/toolset_grafana_tempo.py
@@ -23,9 +23,12 @@ from holmes.plugins.toolsets.grafana.tempo_api import (
     query_tempo_traces,
 )
 from holmes.plugins.toolsets.grafana.trace_parser import format_traces_list
-from holmes.plugins.toolsets.utils import get_param_or_raise, process_timestamps_to_int
 from holmes.plugins.toolsets.logging_utils.logging_api import (
     DEFAULT_TIME_SPAN_SECONDS,
+)
+from holmes.plugins.toolsets.utils import (
+    get_param_or_raise,
+    process_timestamps_to_int,
 )
 
 TEMPO_LABELS_ADD_PREFIX = load_bool("TEMPO_LABELS_ADD_PREFIX", True)
