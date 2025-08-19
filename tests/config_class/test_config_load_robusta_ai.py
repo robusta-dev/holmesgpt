0caf2d79ac8c5472ede6853e7298b241861c47a8
avi@robusta.dev <avi@robusta.dev>
2025-08-19 11:43:53 +0300

    fixing tests

--- tests/config_class/test_config_load_robusta_ai.py
+++ tests/config_class/test_config_load_robusta_ai.py
@@ -1,31 +1,33 @@
 from unittest.mock import patch
 from holmes.config import Config
 
+ROBUSTA_MODEL_NAME = "Robusta/gpt-4o"
+
 
 @patch("holmes.config.ROBUSTA_AI", True)
 def test_cli_not_loading_robusta_ai(monkeypatch):
     config = Config.load_from_file(None)
-    assert "Robusta" not in config._model_list
+    assert ROBUSTA_MODEL_NAME not in config._model_list
 
 
 @patch("holmes.config.ROBUSTA_AI", True)
 def test_server_loads_robusta_ai_when_true(monkeypatch):
     config = Config.load_from_env()
-    assert "Robusta" in config._model_list
+    assert ROBUSTA_MODEL_NAME in config._model_list
 
 
 @patch("holmes.config.ROBUSTA_AI", None)
 def test_server_loads_robusta_ai_when_not_exists_and_not_other_models(monkeypatch):
     config = Config.load_from_env()
     assert len(config._model_list) == 1
-    assert "Robusta" in config._model_list
+    assert ROBUSTA_MODEL_NAME in config._model_list
 
 
 @patch("holmes.config.ROBUSTA_AI", False)
 def test_server_not_loads_robusta_ai_when_false(monkeypatch):
     config = Config.load_from_env()
     assert len(config._model_list) == 0
-    assert "Robusta" not in config._model_list
+    assert ROBUSTA_MODEL_NAME not in config._model_list
 
 
 @patch("holmes.config.ROBUSTA_AI", True)
@@ -36,7 +38,7 @@ def test_server_not_loads_robusta_ai_when_false(monkeypatch):
 def test_server_loads_robusta_ai_when_true_and_model_list_exists(monkeypatch):
     config = Config.load_from_env()
     assert "existing_model" in config._model_list
-    assert "Robusta" in config._model_list
+    assert ROBUSTA_MODEL_NAME in config._model_list
 
 
 @patch("holmes.config.ROBUSTA_AI", False)
@@ -47,7 +49,7 @@ def test_server_loads_robusta_ai_when_true_and_model_list_exists(monkeypatch):
 def test_server_not_loads_robusta_ai_when_false_and_model_list_exists(monkeypatch):
     config = Config.load_from_env()
     assert "existing_model" in config._model_list
-    assert "Robusta" not in config._model_list
+    assert ROBUSTA_MODEL_NAME not in config._model_list
 
 
 @patch("holmes.config.ROBUSTA_AI", None)
@@ -58,21 +60,21 @@ def test_server_not_loads_robusta_ai_when_false_and_model_list_exists(monkeypatc
 def test_server_not_loads_robusta_ai_when_no_env_var_and_model_list_exists(monkeypatch):
     config = Config.load_from_env()
     assert "existing_model" in config._model_list
-    assert "Robusta" not in config._model_list
+    assert ROBUSTA_MODEL_NAME not in config._model_list
 
 
 @patch("holmes.config.ROBUSTA_AI", True)
 def test_server_loads_robusta_ai_when_model_var_exists(monkeypatch):
     monkeypatch.setenv("MODEL", "some_model")
     config = Config.load_from_env()
-    assert "Robusta" in config._model_list
+    assert ROBUSTA_MODEL_NAME in config._model_list
 
 
 @patch("holmes.config.ROBUSTA_AI", None)
 def test_server_not_loads_robusta_ai_when_model_var_exists_and_no_env_var(monkeypatch):
     monkeypatch.setenv("MODEL", "some_model")
     config = Config.load_from_env()
-    assert "Robusta" not in config._model_list
+    assert ROBUSTA_MODEL_NAME not in config._model_list
 
 
 @patch("holmes.config.ROBUSTA_AI", False)
@@ -81,4 +83,4 @@ def test_server_not_loads_robusta_ai_when_model_var_exists_and_false_env_var(
 ):
     monkeypatch.setenv("MODEL", "some_model")
     config = Config.load_from_env()
-    assert "Robusta" not in config._model_list
+    assert ROBUSTA_MODEL_NAME not in config._model_list
