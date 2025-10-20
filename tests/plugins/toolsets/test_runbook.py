2601c5c7f34886240a2e6a4a60c7ab2e22d52472
avi@robusta.dev <avi@robusta.dev>
2025-10-20 08:53:58 +0300

    runbook pytest fix

--- tests/plugins/toolsets/test_runbook.py
+++ tests/plugins/toolsets/test_runbook.py
@@ -9,13 +9,17 @@ from tests.conftest import create_mock_tool_invoke_context
 def test_RunbookFetcher():
     runbook_fetch_tool = RunbookFetcher(RunbookToolset(dal=None))
     result = runbook_fetch_tool._invoke(
-        {"runbook_id": "wrong_runbook_path.md", "type": "md_file"}, context=create_mock_tool_invoke_context()
+        {"runbook_id": "wrong_runbook_path.md", "type": "md_file"},
+        context=create_mock_tool_invoke_context(),
     )
     assert result.status == StructuredToolResultStatus.ERROR
     assert result.error is not None
 
     result = runbook_fetch_tool._invoke(
-        {"runbook_id": "networking/dns_troubleshooting_instructions.md", "type": "md_file"},
+        {
+            "runbook_id": "networking/dns_troubleshooting_instructions.md",
+            "type": "md_file",
+        },
         context=create_mock_tool_invoke_context(),
     )
 
@@ -24,7 +28,10 @@ def test_RunbookFetcher():
     assert result.data is not None
     assert (
         runbook_fetch_tool.get_parameterized_one_liner(
-            {"runbook_id": "networking/dns_troubleshooting_instructions.md", "type": "md_file"}
+            {
+                "runbook_id": "networking/dns_troubleshooting_instructions.md",
+                "type": "md_file",
+            }
         )
         == "Runbook: Fetch Runbook networking/dns_troubleshooting_instructions.md"
     )
